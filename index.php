6797fa5c82eea8404740988c6ba1068980269fb8
yuyuyu393 <2831570002@qq.com>
2026-07-29 17:13:14 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $fetch_url = rtrim($target_url, '/') . $request_uri;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $fetch_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 if(isset($_SERVER['HTTP_USER_AGENT'])) {
